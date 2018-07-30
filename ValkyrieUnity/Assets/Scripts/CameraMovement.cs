27ff742970948e53027202491462f5654ef13eff
Mayank Bagchi <mbagchi17@gmail.com>
2018-07-31 00:22:34 +0530

    2 functionalities on Camera
    1. The camera follows the player after a set range, else remains stationary.
    2. Camera doesn't go out of bounds, ie, clamps when near the boundaries, thus, not displaying nything outside the level.

--- ValkyrieUnity/Assets/Scripts/CameraMovement.cs
+++ ValkyrieUnity/Assets/Scripts/CameraMovement.cs
@@ -1,17 +1,33 @@
 ﻿using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
-	public Transform circle;
-	
-	// Update is called once per frame
-	void Update () {
-		if (transform.position.x >= -1.4 && transform.position.x <= 194.75) {
-			if (circle.position.x > transform.position.x) {
-				transform.position = new Vector3 (circle.position.x, transform.position.y, transform.position.z);
+	public Transform target;
+	public float xBound, speed=0.01f;	
+	public Vector3 offset;
+	Vector2 min,max;
+	float camHalfWidth;
+	public BoxCollider2D camBound;
+	Camera cam;
+
+	void Start()
+	{
+		cam = GetComponent<Camera> ();
+		camHalfWidth = cam.orthographicSize * ((float) Screen.width / Screen.height);
+		min= camBound.bounds.min;
+		max= camBound.bounds.max;
+	}
+
+	void FixedUpdate () {
+			if ((target.position - transform.position).magnitude > xBound) {
+				Vector3 desiredPos = target.position + offset;
+				Vector3 smoothedPos = Vector3.Lerp (transform.position, desiredPos, speed);
+				transform.position = smoothedPos;
 			}
-			else if (transform.position.x > 194.75) {
-					transform.position = new Vector3 (194.75f, transform.position.y, transform.position.z);
-			}
-		} 
+
+		//Clamping Camera withing the Level Bounds
+		transform.position = new Vector3 (
+			Mathf.Clamp (transform.position.x, min.x + camHalfWidth, max.x - camHalfWidth),
+			Mathf.Clamp (transform.position.y, min.y + cam.orthographicSize, max.y - cam.orthographicSize),
+			transform.position.z);
 	}
 }
